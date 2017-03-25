7e6af725a7ca6cc41f6dbe4c5e0573330542b605
Rachid Azzi <rachid.azzi@almacgroup.com>
2017-03-25 11:35:09 -0400

    Implemented paging for Video type media. Prevent image Url fetch when media type isn't Image.

--- TeachMeArabic/app/media/media.controller.js
+++ TeachMeArabic/app/media/media.controller.js
@@ -10,17 +10,13 @@
 
     function MediaController(dataFactory, $routeParams, $sce) {
         var vm = this;
-        vm.API = null;
-
-        vm.onPlayerReady = function (API) {
-            vm.API = API;
-        };
+        vm.VideoApi = null;
 
         vm.mediaTypes = { Text: 0, Html: 1, Image: 2, Video: 3 };
         vm.isLoading = true;
         vm.mediaDetailsPanelIsVisible = true;
-        vm.searchPanelIsVisible = true;
 
+        // functions called from html
         vm.firstPage = firstPage;
         vm.lastPage = lastPage;
         vm.nextPage = nextPage;
@@ -28,7 +24,9 @@
         vm.goToPage = goToPage;
         vm.toggleMediaDetailsPanel = toggleMediaDetailsPanel;
         vm.gotoChapterStartingPage = gotoChapterStartingPage;
+        vm.onPlayerReady = onPlayerReady;
 
+        // Get media data from the server
         getMedia($routeParams.id);
 
         function getMedia(id) {
@@ -45,37 +43,48 @@
               });
         }
 
+        function gotoChapterStartingPage(chapter) {
+            if (chapter.startingPage === vm.media.page) return;
+            vm.media.page = chapter.startingPage;
+            setCurrentVideoBasedOnPage();
+        }
+
+        // Video supporting functions
         function configureVideo() {
             vm.config = {
                 preload: "none",
                 sources: [
                     { src: $sce.trustAsResourceUrl(vm.media.contentPages[vm.media.page - 1].content), type: "video/mp4" }
                 ],
-                tracks: [
-                    {
-                        src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-                        kind: "subtitles",
-                        srclang: "en",
-                        label: "English",
-                        default: ""
-                    }
-                ],
+                //tracks: [
+                //    {
+                //        src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+                //        kind: "subtitles",
+                //        srclang: "en",
+                //        label: "English",
+                //        default: ""
+                //    }
+                //],
                 theme: {
                     url: "Content/videogular.css"
                 }
             };
         }
 
-        function gotoChapterStartingPage(chapter) {
-            vm.media.page = chapter.startingPage;
-            if (vm.media.mediaType === vm.mediaTypes.Video) {
-                vm.API.stop();
-                vm.config.sources = [
-                    { src: $sce.trustAsResourceUrl(vm.media.contentPages[vm.media.page - 1].content), type: "video/mp4" }
-                ];
-            }
+        function onPlayerReady(videoApi) {
+            vm.VideoApi = videoApi;
+        };
+
+
+        function setCurrentVideoBasedOnPage() {
+            if (vm.media.mediaType !== vm.mediaTypes.Video) return;
+            vm.VideoApi.stop();
+            vm.config.sources = [
+                { src: $sce.trustAsResourceUrl(vm.media.contentPages[vm.media.page - 1].content), type: "video/mp4" }
+            ];
         }
 
+        // Paging supporting functions
         function goToPage() {
             if (isNaN(vm.pageNumber) || vm.pageNumber < 1 || vm.pageNumber > vm.media.pages) vm.pageNumber = vm.media.page;
             else vm.media.page = vm.pageNumber;
@@ -83,20 +92,24 @@
 
         function firstPage() {
             vm.media.page = 1;
+            setCurrentVideoBasedOnPage();
         }
 
         function lastPage() {
             vm.media.page = vm.media.pages;
+            setCurrentVideoBasedOnPage();
         }
 
         function nextPage() {
             vm.media.page++;
             if (vm.media.page > vm.media.pages) vm.media.page = 1;
+            setCurrentVideoBasedOnPage();
         }
 
         function previousPage() {
             vm.media.page--;
             if (vm.media.page < 1) vm.media.page = vm.media.pages;
+            setCurrentVideoBasedOnPage();
         }
 
         function toggleMediaDetailsPanel() {
